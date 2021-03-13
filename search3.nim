73b347a8b04a53ffe5c9195b47021459a29490f7
murata <murata@kmc.gr.jp>
2021-03-13 20:04:16 +0900

    search 3

--- search3.nim
+++ search3.nim
@@ -1,9 +1,10 @@
 import tables, strformat
 # f(x,y) の結果(f(0,0),f(0,1),f(0,2),f(1,0)...f(2,2))を返す
 var funs = initTable[string, string]()
-funs["000111222"] = "x"
-funs["012012012"] = "y"
-funs["222222222"] = "2"
+funs["000000000111111111222222222"] = "x"
+funs["000111222000111222000111222"] = "y"
+funs["012012012012012012012012012"] = "z"
+funs["111111111111111111111111111"] = "1"
 while true:
   var next = funs
   var preLen = next.len
@@ -15,22 +16,17 @@ while true:
       for i in 0..<k1.len:
         let c1 = k1[i]
         let c2 = k2[i]
-        if c1 == '0' and c2 == '0': newK &= '1'
-        if c1 == '0' and c2 == '1': newK &= '0'
-        if c1 == '0' and c2 == '2': newK &= '0'
-        if c1 == '1' and c2 == '0': newK &= '1'
-        if c1 == '1' and c2 == '1': newK &= '0'
-        if c1 == '1' and c2 == '2': newK &= '2'
-        if c1 == '2' and c2 == '0': newK &= '2'
-        if c1 == '2' and c2 == '1': newK &= '2'
-        if c1 == '2' and c2 == '2': newK &= '1'
+        if c1 == c2: newK &= '0'
+        elif c2 == '0': newK &= c1
+        elif c1 != '0': newK &= c2
+        elif c2 == '1': newK &= '2'
+        else: newK &= '1'
       if newK in next: continue
-      let newV = fmt"crz({v1},{v2})"
+      let newV = fmt"{v1}-({v2})"
       next[newK] = newV
   if next.len == preLen : break
   funs = next
   echo next.len
-  if next.len > 15000: break
 proc checkAll() =
   proc check(k, ok: string) : bool =
     for i, c in k:
@@ -38,8 +34,8 @@ proc checkAll() =
       if k[i] != ok[i]: return false
     return true
   for k, v in funs:
-    if k.check("000000000"):
+    if k.check("00*01****01*11*************"):
       echo fmt"OK! {k} : {v}"
   echo "check failed..."
-# echo funs
+echo funs
 checkAll()
