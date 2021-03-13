f00bf6db28cc1879b0a27db0b691c2c92ebc8c52
murata <murata@kmc.gr.jp>
2021-03-13 17:50:06 +0900

    ストアとかもできる

--- dis.nim
+++ dis.nim
@@ -15,11 +15,21 @@ proc load(code: string): seq[int] =
     elif c == '(': isComment = true
     else: quit "invalid character : " & c
 
+proc debugPrint(str:string) =
+  stderr.writeLine str
+  # discard
+
 proc printMemory(M:seq[int]) =
   var l = 0
   for i, m in M:
     if m != 0: l = i
-  stderr.writeLine fmt"size:{l}\n{M[0..<l]}"
+  debugPrint fmt"size:{l}\n{M[0..<l]}"
+
+proc tritsub(a,b:int) : int =
+  for p in [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683]:
+    result += (a div p mod 3 - b div p mod 3 + 3) mod 3 * p
+
+proc `--`(a,b: int) : int = tritsub(a,b)
 
 proc exec(base: seq[int]) =
   var M = base
@@ -30,7 +40,7 @@ proc exec(base: seq[int]) =
   var c = 0
   var d = 0
   while true:
-    stderr.writeLine fmt"{M[c].chr}: c:{c} a:{a} d:{d} M[d]:{M[d]}"
+    debugPrint fmt"{M[c].chr}: c:{c} a:{a} d:{d} M[d]:{M[d]}"
     case M[c]:
     of '*'.int: d = M[d]
     of '^'.int: c = M[d]
@@ -38,14 +48,11 @@ proc exec(base: seq[int]) =
       M[d] = M[d] div 3 + (M[d] mod 3) * 19683
       a = M[d]
     of '|'.int:
-      var sum = 0
-      for p in [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683]:
-        sum += (a div p mod 3 - M[d] div p mod 3 + 3) mod 3 * p
-      M[d] = sum
+      M[d] = tritsub(a, M[d])
       a = M[d]
     of '{'.int:
       if a == 59048: return
-      stderr.writeLine fmt"OUTPUT: {a}({a.chr})"
+      debugPrint fmt"OUTPUT: {a}({a.chr})"
       stdout.write a.char
     of '}'.int:
       if inputI >= input.len:
@@ -71,25 +78,76 @@ proc preProcess(code: string) : string =
     .replace("R0", ">__*") # d=34: M[34]をROR
     .replace("R1", "_>_*") # d=34: M[35]をROR
     .replace("R2", "__>*") # d=34: M[36]をROR
-    .replace("F1", ">||*") # d=34: 謎1
-    .replace("F2", ">_|*") # d=34: 謎2
-    .replace("F3", ">|_*") # d=34: 謎3
-    .replace("F4", "__|*") # d=34: 謎4
+    .replace("F1", ">||*") # d=34: 謎1(for hello world)
+    .replace("F2", ">_|*") # d=34: 謎2(for hello world)
+    .replace("F3", ">|_*") # d=34: 謎3(for hello world)
+    .replace("F4", "__|*") # d=34: 謎4(for hello world)
     .replace("P", "{__*")  # d=34: putc(M[34]) (P+R2やPPR2などは結合できるのでたまに結合される)
     .replace("\n","").replace(" ","").strip()
   echo result
+  echo result.len
 
-let cat = fmt"""load jump {"_".repeat(32)} getc putc load jump {"_".repeat(5)} halt {"_".repeat(2)} load"""
 # メモリとコードを同一に置くのはむずすぎるので分ける。
 # d = 34,35,36,37,... をメモリとすることで有効活用(sub putc halt...で初期化). 多分長さ50程度は使用できる
 # d, つまり見ているメモリ番地が毎回+1されるのでうまく回す。*を呼ぶとd=33へ戻されるのがミソ。
-let hello = fmt"""
-jump 33 {"_".repeat(32)}
-124 123 33 33 33 {"33 ".repeat(56)} load (# メモリ)
+# M[d] はともかく, a も変化する(>|で)ので、単純に置換はできない。 a は保存したが一瞬だけ覚えてる値(for minus)と考えるといい
+# | 演算は、ビットごとに独立で、10ビットあるので、うまく使う。
+# 入力が取れなくなると終了するので、16倍して死ぬならループを作る(jumpするだけ？)
+# getc_x(d) := M[d+1]==0 条件で      : a = M[d+1] = getc
+# set_x(d)  := M[d+1]==0 条件で      : M[d+1] = a
+# getc_s(n) := cls_s(n+1) して getc_x(n) で : a = M[n+1] = getc
+# setc_s(n) := cls_s(n+1) して set_x(n) で : M[n+1] = a
+
+# 以下 d:=34の前提とする
+# a = M[n] = M[n] >> 1
+var memorySize = 3 # 40くらいまでだがコード長(と実行時間)が線形に増える
+proc nop(): string = "_".repeat(memorySize) & "*"
+# a = M[n] = M[n] >> 1
+proc tritrot_n(n:int) : string =
+  result = nop()
+  result[n] = '>'
+# a = M[n] = a -- M[n]
+proc tritsub_n(n:int) :string =
+  result = nop()
+  result[n] = '|'
+# a = a; putc(a)
+proc putc_a() : string =
+  result = nop()
+  result[0] = '{'
+# a = getc()
+proc getc_a() : string =
+  result = nop()
+  result[0] = '}'
+# a = M[n]
+proc load_n(n:int): string = tritrot_n(n).repeat(10)
+# a = M[n] = 0
+proc clear_n(n:int): string = load_n(n) & tritsub_n(n)
+# a = M[n] = getc()
+proc getc_n(n:int): string = clear_n(n) & getc_a() & tritsub_n(n)
+# a = M[n]; M[m] = M[n];
+proc cp_n_m(n, m:int): string = clear_n(m) & load_n(n) & tritsub_n(m)
+
+# 48 = 1210
+# 49 = 1211
+# a,bを--のみで以下にしたい
+#   0--b--b--a--a: 00:0 01:1 10:1 11:2
+#   0--b--a: 00:0 01:2 10:2 11:1
+# A,c を--のみで以下にしたい
+# 00:0 01:0 10:0 11:1 20:1 21:1 (000111)もしくは
+# 00:0 01:0 10:1 11:1 20:0 21:1 (001101)
+# for a in [0,1,2]:
+#   for b in [0,1]:
+#     let x = b--a--(a--b)
+#     echo fmt"{a}{b}:{x}"
+# if true: quit ""
+# M[0]=getc M[1]=getc M[2]=getc getc
+# M[0]=getc--M[0]...
+const prefix = fmt""" jump 33 {"_".repeat(32)} {"33 ".repeat(61)} load"""
+let cat = fmt"""load jump {"_".repeat(32)} getc putc load jump {"_".repeat(5)} halt {"_".repeat(2)} load"""
+let hello = fmt"""jump 33 {"_".repeat(32)} 124 123 {"33 ".repeat(59)} load
 R1 F3 R1 R1 R1 R1 R1 R1 R1 R1 R1 R0 |_>* F4 |||* R2 F1 F2 R2 R2 R2 R2 R2 R2 R2 P
 R2 F2 F1 R2 F1 F1 R2 R2 R2 F2 F1 R2 R2 R2 R2 R2 R2 P
-F1 F2 R2 R2 F1 F1 R2 F2 F1 R2 R2 R2 R2 R2 R2 R2 P
-P
+F1 F2 R2 R2 F1 F1 R2 F2 F1 R2 R2 R2 R2 R2 R2 R2 P P
 R2 F1 F2 R2 R2 R2 R2 R2 R2 R2 R2 R2 P
 R2 F2 F1 R2 F2 F1 R2 R2 R2 R2 R2 R2 R2 R2 R2 P
 R2 F1 F2 >|>* F1 F2 R2 R2 R2 R2 R2 R2 R2 R2 P
@@ -100,5 +158,13 @@ R2 F3 F1 F2 R2 R2 R2 R2 R2 R2 R2 R2 R2 P
 R2 F4 >F4 >F4 F4 R2 R2 R2 R2 R2 R2 R2 R2 R2 F2 F1 P
 R2 F4 >F4 >F4 P
 F4 R2 F1 F1 putc halt"""
-let code = hello
+let mine = fmt"""{prefix}
+{getc_n(0)} {cp_n_m(0,1)} {load_n(1)} {putc_a()}
+{getc_n(0)} {cp_n_m(0,1)} {load_n(1)} {putc_a()}
+{getc_n(0)} {cp_n_m(0,1)} {load_n(1)} {putc_a()}
+{getc_n(0)} {cp_n_m(0,1)} {load_n(1)} {putc_a()}
+{getc_n(0)} {cp_n_m(0,1)} {load_n(1)} {putc_a()}
+halt
+"""
+let code = mine
 code.preProcess().load().exec()
